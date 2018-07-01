d2664369cb6e877f8e4e38e3ff438d239eb48de0
Christopher Elwell <elwell.christopher@gmail.com>
2018-06-30 19:27:47 -0700

    comment

--- ClearCoinAdExchange.sol
+++ ClearCoinAdExchange.sol
@@ -142,7 +142,8 @@ contract ClearCoinAdExchange {
         
         emit adSlotActivated(msg.sender);
     }
-    
+
+    // only owner can submit tracked clicks (from ad server)
     function trackClick(address line_item_address, address ad_slot_address) public onlyOwner {
         emit clickTracked(line_item_address, ad_slot_address);
     }
